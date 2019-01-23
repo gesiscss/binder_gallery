55a585b3a13cc0cd83d0b8957d95794f77fa1484
Kenan Erdogan <kenan.erdogan@gesis.org>
2019-01-23 14:34:44 +0100

    mv manage.py into project dir

--- manage.py
+++ manage.py
@@ -1,9 +1,6 @@
-import sys
-print(sys.path)
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from .app import app
-from .models import db
+from binder_gallery.app import app, db
 
 
 migrate = Migrate(app, db)
