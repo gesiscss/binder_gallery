864f95f341769c67ab2ba1dd31aa684793a3e857
Kenan Erdogan <kenan.erdogan@gesis.org>
2019-03-22 15:34:47 +0100

    calculate colspan for description row

--- binder_gallery/static/description_row.js
+++ binder_gallery/static/description_row.js
@@ -1,21 +1,25 @@
-$(document).ready(function() {
+// https://datatables.net/examples/api/row_details.html
+$(document).ready(function () {
     // Add event listener for opening and closing description row
     $('tbody').on('click', 'tr', function () {
         var description = $(this).data().description;
         //console.log(description);
 
-        if ( $(this).hasClass('shown') && $(this).next().hasClass('description_row') ) {
+        if ($(this).hasClass('shown') && $(this).next('tr').hasClass('description_row')) {
             // remove description row
             $(this).removeClass('shown');
-            $(this).next().remove();
+            $(this).next('tr.description_row').remove();
         }
         else if (description) {
             // add description row
             $(this).addClass('shown');
-            var description_row = '<tr class="description_row"><td colspan="5" style="padding-left:25px;">' +
-                                    description +
-                                    '</td></tr>';
+            var description_row = '<tr class="description_row"><td colspan="' + $(this).find('td').length +
+                                  '" style="padding-left:25px;">' +
+                                  description +
+                                  '</td></tr>';
             $(this).after(description_row);
         }
-    } );
+        // don't return anything to allow default behaviours, such as link click
+        // return false;
+    });
 });
