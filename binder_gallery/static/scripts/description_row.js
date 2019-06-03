694d7ff1793ca10b16cfb3d38a109123b99b44cb
Kenan Erdogan <kenan.erdogan@gesis.org>
2019-06-03 08:53:02 +0200

    dont toggle desc row when click on links

--- binder_gallery/static/scripts/description_row.js
+++ binder_gallery/static/scripts/description_row.js
@@ -1,25 +1,27 @@
 // https://datatables.net/examples/api/row_details.html
 $(document).ready(function () {
     // Add event listener for opening and closing description row
-    $('tbody').on('click', 'tr', function () {
-        var description = $(this).data().description;
-        //console.log(description);
+    $('tbody').on('click', 'tr', function (e) {
+        if (e.target.nodeName !== "A" && e.target.nodeName !== "IMG") {
+            var description = $(this).data().description;
+            //console.log(description);
 
-        if ($(this).hasClass('shown') && $(this).next('tr').hasClass('description_row')) {
-            // remove description row
-            $(this).removeClass('shown');
-            $(this).next('tr.description_row').remove();
+            if ($(this).hasClass('shown') && $(this).next('tr').hasClass('description_row')) {
+                // remove description row
+                $(this).removeClass('shown');
+                $(this).next('tr.description_row').remove();
+            }
+            else if (description) {
+                // add description row
+                $(this).addClass('shown');
+                var description_row = '<tr class="description_row"><td colspan="' + $(this).find('td').length +
+                                      '" style="padding-left:25px;">' +
+                                      description +
+                                      '</td></tr>';
+                $(this).after(description_row);
+            }
+            // don't return anything to allow default behaviours, such as link click
+            // return false;
         }
-        else if (description) {
-            // add description row
-            $(this).addClass('shown');
-            var description_row = '<tr class="description_row"><td colspan="' + $(this).find('td').length +
-                                  '" style="padding-left:25px;">' +
-                                  description +
-                                  '</td></tr>';
-            $(this).after(description_row);
-        }
-        // don't return anything to allow default behaviours, such as link click
-        // return false;
     });
 });
